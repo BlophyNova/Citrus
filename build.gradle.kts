22161aec89829a292d682c433cd54dd354a021dd
MojaveHao <HTTcode2020@126.com>
2025-01-09 18:40:28 +0800

    feat: 登录缓存

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ repositories {
 
 dependencies {
     implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-netty-jvm")
     implementation("io.ktor:ktor-server-auth-jvm")
     implementation("io.ktor:ktor-server-auth-jwt-jvm")
     implementation("io.ktor:ktor-server-sessions")
